999acbd66e07ae62fa0c8771fef72a7a92cac9bf
Remington Rohel <rar129@usask.ca>
2024-09-17 23:56:36 -0600

    Further work on the hard solution.

--- submissions/rohel_hard.py
+++ submissions/rohel_hard.py
@@ -9,20 +9,31 @@ class Strategy(Hard):
 
     def solve(game):
         # Start with gathering some information about Alice and Bob.
-        truth_dict = {
-            Alice: {},
-            Bob: {},
-            Charlie: {},
-            Dan: {},
-        }
-        results = []
-        results.append(game.get_response(Alice.ask(True)))
-        results.append(game.get_response(Alice.ask(False)))
-        results.append(game.get_response(Bob.ask(True)))
-        results.append(game.get_response(Bob.ask(False)))
+        bool_map = {}
+        game.guess[Alice] = Engg
+        game.guess[Bob] = Phil
+        game.guess[Charlie] = Math
+        game.guess[Dan] = Phys
 
-        truth_dict[Alice] = {True: results[0], False: results[1]}
-        truth_dict[Bob] = {True: results[2], False: results[3]}
+        def assign_math_phys_pair(person_x, person_y, truth_word=None):
+            if truth_word is None:
+                truth_word = bool_map[person_x]
+            if game.person_is_mathematician(person_x, truth_word):
+                game.guess[person_x] = Math
+                game.guess[person_y] = Phys
+            else:
+                game.guess[person_x] = Phys
+                game.guess[person_y] = Math
+
+        results = [
+            game.get_response(Alice.ask(True)),
+            game.get_response(Alice.ask(False)),
+            game.get_response(Bob.ask(True)),
+            game.get_response(Bob.ask(False))
+        ]
+
+        bool_map[Alice] = results[0]
+        bool_map[Bob] = results[2]
 
         # Someone answering the same response twice indicates they are a Philosopher or Engineer
         if results[0] == results[1] and results[2] == results[3]:
@@ -38,15 +49,10 @@ class Strategy(Hard):
                 # We don't need to know whether the control_word means True or False to Charlie, 
                 # we only care if their response matches the control_word.
                 # If so, the other half of the question is True.
-                if game.get_response(control_question(Charlie.studies(Math))) == control_word:
-                    game.guess[Charlie] = Math
-                    game.guess[Dan] = Phys
-                else:
-                    game.guess[Charlie] = Phys
-                    game.guess[Dan] = Math
+                assign_math_phys_pair(Charlie, Dan, truth_word=control_word)
 
                 # Now figure out who is what between Alice and Bob
-                if game.get_response(control_question(Alice.studies(Phil))) == control_word:
+                if game.get_response(Charlie.ask(control_question(Alice.studies(Phil)))) == control_word:
                     game.guess[Alice] = Phil
                     game.guess[Bob] = Engg
                 else:
@@ -56,13 +62,13 @@ class Strategy(Hard):
             if len(remaining_responses) == 2:
                 # the remaining response is part of the set of Mathematician/Physicist responses
                 
-                reply = game.get_response(control_question(Charlie.studies(Math)))
+                reply = game.get_response(Charlie.ask(control_question(Charlie.studies(Math))))
                 if reply == control_word:
                     # Only happens if control_word means True, and the question is True
                     game.guess[Charlie] = Math
                     game.guess[Dan] = Phys
                     # Still need to determine between Alice and Bob who is the Engineer/Philosopher
-                    if game.get_response(control_question(Alice.studies(Engg))) == control_word:
+                    if game.get_response(Charlie.ask(control_question(Alice.studies(Engg)))) == control_word:
                         game.guess[Alice] = Engg
                         game.guess[Bob] = Phil
                     else:
@@ -72,6 +78,7 @@ class Strategy(Hard):
                     # The reply did not match control_word, Charlie does not study Math
                     game.guess[Charlie] = Phys
                     game.guess[Dan] = Math
+                    # todo: What do Alice and Bob study?
 
             return  # All done here
 
@@ -81,11 +88,89 @@ class Strategy(Hard):
 
         elif results[2] == results[3]:
             # Bob gave identical answers, he is either the Engineer or Philosopher
-            pass
+            bool_map[Charlie] = game.get_response(Charlie.ask(True))
+            if bool_map[Charlie] in results and len(set(results)) == 2:
+                # Either Alice/Bob are Engg/Phil, or Alice is Math/Phys.
+                # Alice:   [Engg,      Math/Phys]
+                # Bob:     [Phil,      Engg/Phil]
+                # Charlie: [Math/Phys, Math/Phys/Phil]
+                # Dan:     [Math/Phys, Math/Phys/Phil]
+                not_bob = {Foo, Bar, Baz} - set(bool_map[Bob])
+                dan_false = game.get_response(Dan.ask(False))
+                if dan_false in not_bob:
+                    # Can only happen if in right column scenario
+                    game.guess[Bob] = Engg
+                    game.guess[Dan] = Phil
+                    assign_math_phys_pair(Alice, Charlie)
+                else:
+                    game.guess[Alice] = Engg
+                    game.guess[Bob] = Phil
+                    assign_math_phys_pair(Charlie, Dan)
+            elif bool_map[Charlie] in results:
+                # Alice:   [Engg,      Math/Phys]
+                # Bob:     [Phil,      Engg]
+                # Charlie: [Math/Phys, Math/Phys/Phil]
+                # Dan:     [Math/Phys, Math/Phys/Phil]
+                if bool_map[Charlie] == bool_map[Bob]:
+                    # Right column scenario
+                    game.guess[Bob] = Engg
+                    if game.get_response(Alice.ask(Charlie.studies(Phil))) == bool_map[Alice]:
+                        game.guess[Charlie] = Phil
+                        assign_math_phys_pair(Alice, Dan)
+                    else:
+                        game.guess[Dan] = Phil
+                        assign_math_phys_pair(Alice, Charlie)
+            else:
+                # Charlie said something new, so either Alice/Bob and Engg/Phil, or Charlie is Phil
+                # In other words, Dan cannot be Phil
+                # If Alice/Bob are Engg/Phil, then whoever said the same thing twice is Phil (in this case, Bob)
+                # If Charlie is Phil, then Bob must be Engg since he repeated a response, so Alice is Math/Phys
+                # Either way, Dan is Math/Phys
+                # So, Alice is [Engg, Math/Phys], Bob is [Phil, Engg], Charlie is [Math/Phys, Phil], Dan is [Math/Phys]
+                if game.get_response(Dan.ask(False)) == bool_map[Charlie]:
+                    # Charlie and Dan are the Math/Physics pair
+                    assign_math_phys_pair(Charlie, Dan)
+                    game.guess[Alice] = Engg
+                    game.guess[Bob] = Phil
+                else:
+                    game.guess[Bob] = Engg
+                    game.guess[Charlie] = Phil
+                    assign_math_phys_pair(Alice, Dan)
 
         elif results[0] == results[3] and results[1] == results[2]:
-            # Alice and Bob may be the Mathematician and Physicist
-            pass
+            # The philosopher is either Charlie or Dan
+            bool_map[Charlie] = game.get_response(Charlie.ask(True))
+            if bool_map[Charlie] not in results[:2]:
+                # Charlie is either the Engineer or the Philosopher
+                bool_map[Dan] = game.get_response(Dan.ask(True))
+                if bool_map[Dan] in results[:2]:
+                    game.guess[Charlie] = Phil
+                    if bool_map[Dan] == bool_map[Alice]:
+                        # Either Dan or Alice is the engineer
+                        if game.person_is_mathematician(Bob, bool_map[Bob]):
+                            game.guess[Bob] = Math
+                        else:
+                            game.guess[Bob] = Phys
+                        # todo: How can we figure the rest out in less turns?
+                    else:
+                        # Either Dan or Bob is the engineer
+                        if game.person_is_mathematician(Alice, bool_map[Alice]):
+                            game.guess[Alice] = Math
+                        else:
+                            game.guess[Alice] = Phys
+                        # todo: How can we figure the rest out in less turns?
+            else:
+                # The philosopher must be Dan
+                game.guess[Dan] = Phil
+                if bool_map[Charlie] == bool_map[Alice]:
+                    # Alice or Charlie is the Engineer
+                    if game.get_response(Bob.ask(Alice.studies(Engg))) == bool_map[Bob]:
+                        game.guess[Alice] = Engg
+                        other = Charlie
+                    else:
+                        game.guess[Charlie] = Engg
+                        other = Alice
+                    assign_math_phys_pair(Bob, other)
 
         else:
             # Two different response sets, one of Alice and Bob is the Engineer
@@ -99,24 +184,12 @@ class Strategy(Hard):
                     # Charlie replies the same no matter what, he is Phil and thus Alice Engg
                     game.guess[Charlie] = Phil
                     game.guess[Alice] = Engg
-                    control_word = truth_dict[Bob][True]
-                    if game.person_is_mathematician(Bob, control_word):
-                        game.guess[Bob] = Math
-                        game.guess[Dan] = Phys
-                    else:
-                        game.guess[Bob] = Phys
-                        game.guess[Dan] = Math
+                    assign_math_phys_pair(Bob, Dan)
                 else:
                     # Charlie is not Phil, therefore Bob/Dan and Engg/Phil and Alice/Charlie are Math/Phys
                     game.guess[Dan] = Phil
                     game.guess[Bob] = Engg
-                    control_word = truth_dict[Alice][True]
-                    if game.person_is_mathematician(Alice, control_word):
-                        game.guess[Alice] = Math
-                        game.guess[Charlie] = Phys
-                    else:
-                        game.guess[Alice] = Phys
-                        game.guess[Charlie] = Math
+                    assign_math_phys_pair(Alice, Charlie)
                     
             elif reply == results[3]:
                 # Charlie's True is Bob's False, possibly Bob/Charlie are Math/Phys, Alice/Dan Engg/Phil
@@ -125,33 +198,17 @@ class Strategy(Hard):
                     # Charlie replies the same no matter what, he is Phil and thus Bob Engg
                     game.guess[Charlie] = Phil
                     game.guess[Bob] = Engg
-                    control_word = truth_dict[Alice][True]
-                    if game.person_is_mathematician(Alice, control_word):
-                        game.guess[Alice] = Math
-                        game.guess[Dan] = Phys
-                    else:
-                        game.guess[Alice] = Phys
-                        game.guess[Dan] = Math
+                    assign_math_phys_pair(Alice, Dan)
                 else:
                     # Charlie is not Phil, therefore Alice/Dan and Engg/Phil and Bob/Charlie are Math/Phys
                     game.guess[Dan] = Phil
                     game.guess[Alice] = Engg
-                    control_word = truth_dict[Bob][True]
-                    if game.person_is_mathematician(Bob, control_word):
-                        game.guess[Bob] = Math
-                        game.guess[Charlie] = Phys
-                    else:
-                        game.guess[Bob] = Phys
-                        game.guess[Charlie] = Math
+                    assign_math_phys_pair(Bob, Charlie)
+
             elif reply == results[0]:
                 # Charlie's True matches Alice's True, these two must be the Engineer/Philosopher
-                control_word = truth_dict[Bob][True]
-                if game.get_response(Bob.ask(control_word.equals(True).iff(Bob.studies(Math)))) == control_word:
-                    game.guess[Bob] = Math
-                    game.guess[Dan] = Phys
-                else:
-                    game.guess[Bob] = Phys
-                    game.guess[Dan] = Math
+                assign_math_phys_pair(Bob, Dan)
+                control_word = bool_map[Bob]
                 if game.get_response(Bob.ask(control_word.equals(True).iff(Alice.studies(Engg)))) == control_word:
                     game.guess[Alice] = Engg
                     game.guess[Charlie] = Phil
@@ -160,13 +217,8 @@ class Strategy(Hard):
                     game.guess[Charlie] = Engg
             elif reply == results[2]:
                 # Charlie's True matches Bob's True, these two must be the Engineer/Philosopher
-                control_word = truth_dict[Alice][True]
-                if game.get_response(Alice.ask(control_word.equals(True).iff(Alice.studies(Math)))) == control_word:
-                    game.guess[Alice] = Math
-                    game.guess[Dan] = Phys
-                else:
-                    game.guess[Alice] = Phys
-                    game.guess[Dan] = Math
+                assign_math_phys_pair(Alice, Dan)
+                control_word = bool_map[Alice]
                 if game.get_response(Alice.ask(control_word.equals(True).iff(Bob.studies(Engg)))) == control_word:
                     game.guess[Bob] = Engg
                     game.guess[Charlie] = Phil
@@ -174,9 +226,7 @@ class Strategy(Hard):
                     game.guess[Bob] = Phil
                     game.guess[Charlie] = Engg
 
-
-    @staticmethod
-    def person_is_mathematician(name, control_word):
+    def person_is_mathematician(game, name, control_word):
         """
         Will determine if a person (that we must know beforehand is a Math/Phys person) is the mathematician
         """
